9d0af9997a19ba71c55bcafb337bdb4ed3ac399f
chibx <111882496+chibx@users.noreply.github.com>
2026-05-15 18:22:48 +0100

    Add the UpgradeToBusiness request struct

--- internal/types/request/users.go
+++ internal/types/request/users.go
@@ -4,7 +4,6 @@ package request
 type RegisterUserRequest struct {
 	FullName string `form:"full_name" json:"full_name" name:"Full Name" validate:"required,lte=40"`
 	Email    string `form:"email" json:"email" name:"Email Address" validate:"required,email"`
-	Phone    string `form:"phone" json:"phone" name:"Phone Number" validate:"required,gte=10"`
 	Password string `form:"password" json:"password" name:"Password" validate:"required,lte=30"`
 }
 
@@ -18,4 +17,12 @@ type UserLoginRequest struct {
 type UpdateUserProfileRequest struct {
 	FullName string `form:"full_name" json:"full_name" name:"Full Name"`
 	Phone    string `form:"phone" json:"phone" name:"Phone Number"`
+}
+
+type UpgradeToBusinessAccountRequest struct {
+	BusinessName string `form:"business_name" json:"business_name"`
+	Phone        string `form:"phone" json:"phone" name:"Phone Number" validate:"required,gte=10"`
+	Address      string `form:"address" json:"address" validate:"required,gte=6"`
+	CAC          string `form:"cac" json:"cac" validate:"required,gte=7"`
+	NIN          string `form:"nin" json:"nin" validate:"required,gte=10"`
 }
